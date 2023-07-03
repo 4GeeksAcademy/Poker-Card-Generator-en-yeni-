aca2e670cc035659b58a5db725b92ef3b36343cb
gokaybalkaya <130371393+gokaybalkaya@users.noreply.github.com>
2023-07-03 16:00:26 +0000

    random card finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.getElementById("card");
+  let suits = ["♠", "♣", "♥", "♦"];
+  let cardNumbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
+
+  let suitIndex = getRandomNumber(0, 3);
+  let cardNumberIndex = getRandomNumber(0, 12);
+
+  let suit = suits[suitIndex];
+  let cardNumber = cardNumbers[cardNumberIndex];
+
+  card.getElementsByClassName("top-left-icon")[0].textContent = suit;
+  card.getElementsByClassName("bottom-right-icon")[0].textContent = suit;
+  card.getElementsByClassName("number")[0].textContent = cardNumber;
 };
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
